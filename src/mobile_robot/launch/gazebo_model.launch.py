a475bc400edf6e9c1d90fdcdfd10a5e98df942a8
techtactio <pkd22it005@gecskp.ac.in>
2025-10-02 17:30:34 +0530

    Initial commit - fresh start

---
+++ src/mobile_robot/launch/gazebo_model.launch.py
@@ -0,0 +1,83 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_ros.actions import Node
+import xacro
+
+
+def generate_launch_description():
+    robotXacroName = 'differential_drive_robot'
+    namePackage = 'mobile_robot'
+    modelFileRelativePath = 'model/robot.xacro'
+
+    # Process robot.xacro
+    pathModelFile = os.path.join(
+        get_package_share_directory(namePackage),
+        modelFileRelativePath
+    )
+    robotDescription = xacro.process_file(pathModelFile).toxml()
+
+    # Gazebo launch file
+    gazebo_rosPackageLaunch = PythonLaunchDescriptionSource(
+        os.path.join(
+            get_package_share_directory('ros_gz_sim'),
+            'launch',
+            'gz_sim.launch.py'
+        )
+    )
+    gazeboLaunch = IncludeLaunchDescription(
+        gazebo_rosPackageLaunch,
+        launch_arguments={
+            'gz_args': '-r -v4 empty.sdf',
+            'on_exit_shutdown': 'true'
+        }.items()
+    )
+
+    # Spawn robot
+    spawnModelNodeGazebo = Node(
+        package='ros_gz_sim',
+        executable='create',
+        arguments=[
+            '-name', robotXacroName,
+            '-topic', 'robot_description'
+        ],
+        output='screen',
+    )
+
+    # Robot state publisher
+    nodeRobotStatePublisher = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        output='screen',
+        parameters=[{
+            'robot_description': robotDescription,
+            'use_sim_time': True
+        }]
+    )
+
+    # Bridge
+    bridge_params = os.path.join(
+        get_package_share_directory(namePackage),
+        'parameters',
+        'bridge_parameters.yaml'
+    )
+    start_gazebo_ros_bridge_cmd = Node(
+        package='ros_gz_bridge',
+        executable='parameter_bridge',
+        arguments=[
+            '--ros-args',
+            '-p',
+            f'config_file:={bridge_params}',
+        ],
+        output='screen',
+    )
+
+    # Return LaunchDescription
+    return LaunchDescription([
+        gazeboLaunch,
+        spawnModelNodeGazebo,
+        nodeRobotStatePublisher,
+        start_gazebo_ros_bridge_cmd
+    ])
